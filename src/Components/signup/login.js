dca8e4ab35c64205028dfa8d722a73a52609f890
Terry Robinson <tdrobinson94@comcast.net>
2020-03-26 23:33:35 -0400

    linked api to signup and login

--- src/Components/signup/login.js
+++ src/Components/signup/login.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import $ from 'jquery';
+import axios from 'axios';
 import Aux from '../layout/Aux';
 import '..//..//SCSS/Signup.scss';
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.changeHandler = this.changeHandler.bind(this)
+        this.submitHandler = this.submitHandler.bind(this)
         this.onStart = this.onStart.bind(this);
     }
 
@@ -17,29 +24,56 @@ class Login extends React.Component {
         this.onStart();
     }
 
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('https://react-calendar-backend-api.herokuapp.com/login', this.state)
+
+            .then(response => {
+                console.log(response)
+                // $('.success-message').addClass('show-success');
+            })
+
+            .catch(error => {
+                console.log(error)
+            })
+
+            this.setState({ 
+                password: ''
+            })
+            $('.signup-form').animate({ scrollTop: 0 }, 500);
+    }
+
     onStart = (e) => {
         $('body, html').animate({ scrollTop: 0}, 500);
     }
     render() {
+        const {  username, password } = this.state
         return (
             <Aux>
                 <div className="signup-form">
                     <div className="form-nav">
                         <h2>Login</h2>
                     </div>
-                    <form>
+                    <form className="signupForm" onSubmit={this.submitHandler} ref={form => this.form = form}>
                         <div className="input-wrapper username">
                             <label>Username</label>
-                            <input type="text" placeholder="Username" />
+                            <input name="username" type="text" placeholder="Username" value={username} onChange={this.changeHandler}/>
                         </div>
                         <div className="input-wrapper email">
                             <label>Password</label>
-                            <input type="password" placeholder="Password" />
+                            <input name="password" type="password" placeholder="Password" value={password} onChange={this.changeHandler}/>
                         </div>
                         <div className="input-wrapper">
                             <button className="login" type="submit">Login</button>
                         </div>
                     </form>
+                    <div class="success-message">Hi {username}, you are now logged in.</div>
+                    <div class="fail-message">Hi {username}, you haven't been logged in. Username or Password is incorrect.</div>
                 </div>
             </Aux>
         );
--- src/Components/signup/signup.js
+++ src/Components/signup/signup.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import $ from 'jquery';
+import axios from 'axios';
 import Aux from '../layout/Aux';
 import '..//..//SCSS/Signup.scss';
 
 class Signup extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            username: '',
+            email: '',
+            password: ''
+        }
+        this.changeHandler = this.changeHandler.bind(this)
+        this.submitHandler = this.submitHandler.bind(this)
         this.onStart = this.onStart.bind(this);
     }
 
@@ -17,36 +27,64 @@ class Signup extends React.Component {
         this.onStart();
     }
 
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('https://react-calendar-backend-api.herokuapp.com/signup', this.state)
+
+        .then(response => {
+            console.log(response)
+        })
+
+        .catch(error => {
+            console.log(error)
+        })
+
+        this.setState({
+            firstname: '',
+            lastname: '',
+            username: '',
+            email: '',
+            password: ''
+        })
+        $('.signup-form').animate({ scrollTop: 0 }, 500);
+    }
+
     onStart = (e) => {
         $('body, html').animate({ scrollTop: 0}, 500);
     }
     render() {
+        const { firstname, lastname, username, email, password } = this.state 
         return (
             <Aux>
                 <div className="signup-form">
                     <div className="form-nav">
                         <h2>Signup</h2>
                     </div>
-                    <form>
+                    <form className="signupForm" onSubmit={this.submitHandler} ref={form => this.form = form}>
                         <div className="input-wrapper item-name">
                             <label>First Name</label>
-                            <input type="text" placeholder="First name" />
+                            <input name="firstname" type="text" placeholder="First name" value={firstname} onChange={this.changeHandler}/>
                         </div>
                         <div className="input-wrapper item-name">
                             <label>Last Name</label>
-                            <input type="text" placeholder="Last Name" />
+                            <input name="lastname" type="text" placeholder="Last Name" value={lastname} onChange={this.changeHandler}/>
                         </div>
                         <div className="input-wrapper username">
                             <label>Username</label>
-                            <input type="text" placeholder="Username" />
+                            <input name="username" type="text" placeholder="Username" value={username} onChange={this.changeHandler}/>
                         </div>
                         <div className="input-wrapper email">
                             <label>Email</label>
-                            <input type="email" placeholder="Email" />
+                            <input name="email" type="email" placeholder="Email" value={email} onChange={this.changeHandler}/>
                         </div>
                         <div className="input-wrapper email">
                             <label>Password</label>
-                            <input type="password" placeholder="Password" />
+                            <input name="password" type="password" placeholder="Password" value={password} onChange={this.changeHandler}/>
                         </div>
                         <div className="input-wrapper">
                             <button className="signup" type="submit">Signup</button>
